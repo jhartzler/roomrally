59f603b97f449c45ed6fe7a819db524ea8ce9de1
Jack Hartzler <john.m.hartzler@gmail.com>
2025-11-26 16:26:06 -0500

    test: model specs

--- spec/models/prompt_instance_spec.rb
+++ spec/models/prompt_instance_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe PromptInstance, type: :model do
   describe 'associations' do
-    it { is_expected.to belong_to(:room) }
+    it { is_expected.to belong_to(:write_and_vote_game) }
     it { is_expected.to belong_to(:prompt) }
     it { is_expected.to have_many(:responses).dependent(:destroy) }
   end
