0585a8888d6a10dea81f0f52488e99ce01767430
Jack Hartzler <john.m.hartzler@gmail.com>
2025-12-22 11:59:11 -0500

    refactor: Remove minimum prompt count validation from PromptPack

--- spec/models/prompt_pack_spec.rb
+++ spec/models/prompt_pack_spec.rb
@@ -9,21 +9,5 @@ RSpec.describe PromptPack, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:game_type) }
-
-    context 'when game_type is Write and Vote' do
-      let(:user) { create(:user) }
-      let(:prompt_pack) { build(:prompt_pack, game_type: 'Write And Vote', user:) }
-
-      it 'is not valid with fewer than 3 prompts' do
-        prompt_pack.prompts = build_list(:prompt, 2)
-        expect(prompt_pack).not_to be_valid
-        expect(prompt_pack.errors[:base]).to include("must have at least 3 prompts for Write and Vote")
-      end
-
-      it 'is valid with 3 or more prompts' do
-        prompt_pack.prompts = build_list(:prompt, 3)
-        expect(prompt_pack).to be_valid
-      end
-    end
   end
 end
