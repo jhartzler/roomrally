fcfedf6e137414e8097fa07f76d13576eb33db59
Jack Hartzler <johnm.hartzler@gmail.com>
2025-12-27 13:57:40 -0500

    style: fix trailing whitespace in backstages controller

--- app/controllers/backstages_controller.rb
+++ app/controllers/backstages_controller.rb
@@ -5,7 +5,7 @@ class BackstagesController < ApplicationController
 
   def show
     Rails.logger.info("DEBUG: Backstage Show - Room: #{@room.code}, Current Game ID: #{@room.current_game_id}, Current Game Type: #{@room.current_game_type}")
-    
+
     if @room.current_game.present?
       @moderation_queue = Response.joins(:prompt_instance)
                                   .where(prompt_instances: { write_and_vote_game_id: @room.current_game.id, round: @room.current_game.round })
