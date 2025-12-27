4136d22a270a08baf1bd3c9dc10b666a117d069a
Jack Hartzler <johnm.hartzler@gmail.com>
2025-12-27 13:56:31 -0500

    refactor: deduplicate views, extract helpers, and optimize queries

--- app/controllers/backstages_controller.rb
+++ app/controllers/backstages_controller.rb
@@ -5,6 +5,15 @@ class BackstagesController < ApplicationController
 
   def show
     Rails.logger.info("DEBUG: Backstage Show - Room: #{@room.code}, Current Game ID: #{@room.current_game_id}, Current Game Type: #{@room.current_game_type}")
+    
+    if @room.current_game.present?
+      @moderation_queue = Response.joins(:prompt_instance)
+                                  .where(prompt_instances: { write_and_vote_game_id: @room.current_game.id, round: @room.current_game.round })
+                                  .where(status: "submitted")
+                                  .order(created_at: :desc)
+    else
+      @moderation_queue = Response.none
+    end
   end
 
   private
