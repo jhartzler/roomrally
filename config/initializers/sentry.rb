9d407ad57dc3d9cc4c5be006be615abea93462d6
Jack Hartzler <johnm.hartzler@gmail.com>
2026-02-20 19:35:20 -0500

    Don't allow sentry to receive alerts from CI tests

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,6 @@
 Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"] || Rails.application.credentials.dig(:sentry, :dsn)
+  config.enabled = !Rails.env.test?
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
 
   # Set traces_sample_rate to 1.0 to capture 100%
